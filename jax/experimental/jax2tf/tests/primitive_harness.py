97a7927da80dd18e1a4590977564c832fdbfa579
Marc van Zee <marcvanzee@google.com>
2021-06-21 03:36:35 -0700

    Implements dynamic_update_slice when enable_xla=False and adds tests.
    PiperOrigin-RevId: 380542030

--- jax/experimental/jax2tf/tests/primitive_harness.py
+++ jax/experimental/jax2tf/tests/primitive_harness.py
@@ -1145,6 +1145,24 @@ for indices, index_oob, indices_name in [
           enable_xla=enable_xla,
           index_oob=index_oob)
 
+# Construct gather harnesses using array indexing and slicing.
+for slices, name in [
+    ((0,), "[0]"),
+    ((0, 1), "[0,1]"),
+    ((slice(0, 10), 2, slice(0, 10)), "[:,:2,:]"),
+    ((slice(2, 5), 5), "[2:5,5]"),
+    ((-1, -5, -200), "[-1,-5,-200]"),
+    ((slice(5, -2), 300), "[5:-2,300]"),
+]:
+  for enable_xla in [False, True]:
+    define(
+        lax.gather_p,
+        f"from_slicing_name={name}_enable_xla={enable_xla}",
+        lambda arr, *s: jnp.array(arr).__getitem__(*s),
+        [_gather_input, StaticArg(slices)],
+        dtype=_gather_input.dtype,
+        enable_xla=enable_xla)
+
 # Directly from lax.gather in lax_test.py.
 for shape, idxs, dnums, slice_sizes, needs_xla in [
     ((5,), np.array([[0], [2]]),
@@ -1986,22 +2004,24 @@ def _make_dynamic_update_slice_harness(name,
                                        start_indices=(1,),
                                        dtype=np.float32,
                                        update_shape=(1,)):
-  define(
-      lax.dynamic_update_slice_p,
-      (
-          f"{name}_operand={jtu.format_shape_dtype_string(shape, dtype)}"  # type: ignore
-          f"_update={jtu.format_shape_dtype_string(update_shape, dtype)}"
-          f"_start_indices={start_indices}"),
-      lax.dynamic_update_slice,
-      [
-          RandArg(shape, dtype),  # type: ignore
-          RandArg(update_shape, dtype),  # type: ignore
-          np.array(start_indices)
-      ],  # type: ignore
-      dtype=dtype,
-      shape=shape,  # type: ignore
-      start_indices=start_indices,  # type: ignore
-      update_shape=update_shape)  # type: ignore
+  for enable_xla in [False, True]:
+    define(
+        lax.dynamic_update_slice_p,
+        (
+            f"{name}_operand={jtu.format_shape_dtype_string(shape, dtype)}"  # type: ignore
+            f"_update={jtu.format_shape_dtype_string(update_shape, dtype)}"
+            f"_start_indices={start_indices}_enablexla={enable_xla}"),
+        lax.dynamic_update_slice,
+        [
+            RandArg(shape, dtype),  # type: ignore
+            RandArg(update_shape, dtype),  # type: ignore
+            np.array(start_indices)
+        ],  # type: ignore
+        dtype=dtype,
+        shape=shape,  # type: ignore
+        start_indices=start_indices,  # type: ignore
+        update_shape=update_shape,  # type: ignore
+        enable_xla=enable_xla)
 
 
 # Test first all dtypes
